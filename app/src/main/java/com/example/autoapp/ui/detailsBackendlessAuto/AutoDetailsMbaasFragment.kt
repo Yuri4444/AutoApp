fa9f439f45fcdbfb94104faff391f316d5c84636
Yurii Berezhnyi <yuriy.b@mobian.studio>
2021-07-02 17:29:11 +0300

    working with view pager

--- app/src/main/java/com/example/autoapp/ui/detailsBackendlessAuto/AutoDetailsMbaasFragment.kt
+++ app/src/main/java/com/example/autoapp/ui/detailsBackendlessAuto/AutoDetailsMbaasFragment.kt
@@ -9,15 +9,16 @@ import androidx.fragment.app.Fragment
 import com.backendless.Backendless
 import com.backendless.async.callback.AsyncCallback
 import com.backendless.exceptions.BackendlessFault
+import com.backendless.persistence.DataQueryBuilder
 import com.example.autoapp.data.model.CollectionList
 import com.example.autoapp.databinding.FragmentAutoDetailsMbaasBinding
 import com.example.autoapp.ui.detailsBackendlessAuto.adapter.SwipeListAdapter
-import com.example.autoapp.utils.constants.Constants
 import com.example.autoapp.utils.mbass.Defaults
+
 
 class AutoDetailsMbaasFragment : Fragment() {
 
-    private var _binding : FragmentAutoDetailsMbaasBinding? = null
+    private var _binding: FragmentAutoDetailsMbaasBinding? = null
     private val binding get() = _binding
     private val adapter by lazy {
         SwipeListAdapter(requireContext())
@@ -29,21 +30,18 @@ class AutoDetailsMbaasFragment : Fragment() {
 
         val args: String? = arguments?.getString("Good")
 
+        val whereClause = "idCar = $args"
+        val queryBuilder = DataQueryBuilder.create()
+        queryBuilder.whereClause = whereClause
+
         Backendless.setUrl(Defaults.SERVER_URL)
         Backendless.initApp(requireContext(), Defaults.APPLICATION_ID, Defaults.API_KEY)
 
         Backendless.Persistence.of(CollectionList::class.java)
-            .find(object : AsyncCallback<List<CollectionList>> {
+            .find(queryBuilder, object : AsyncCallback<List<CollectionList>> {
                 override fun handleResponse(response: List<CollectionList>) {
 
-
-                        val concreteList = response.filter { it.idCar == args }
-
-
-
-
-
-                    adapter.setData(concreteList)
+                    adapter.setData(response)
                     Log.e("BackendlessDetails", "success -> ${response.count()}")
                 }
 
